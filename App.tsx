1d9301bc7046f31c9903527333398e847ce7c4d6
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2025-02-05 22:07:59 +0900

    [fix] - build 안되던거 수정

--- App.tsx
+++ App.tsx
@@ -1,33 +1,58 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Dimensions, useColorScheme} from 'react-native';
+import {SafeAreaView, StyleSheet, Dimensions} from 'react-native';
 import WebView from 'react-native-webview';
-import {Colors} from "react-native/Libraries/NewAppScreen";
+// import {NavigationContainer} from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
+// const RootStack = createStackNavigator();
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function WebviewContainer() {
+  // const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  // const backgroundStyle = {
+  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  // };
 
   return (
-      <SafeAreaView style={styles.container}>
-        <WebView
-            style={styles.webview}
-            originWhitelist={['*']}
-            source={{ uri: 'http://192.168.0.10:5173/' }}
-            onLoad={() => console.log('WebView loaded successfully')}
-            javaScriptEnabled={true}  // JavaScript 활성화
-            domStorageEnabled={true}  // DOM Storage 활성화
-            onLoadStart={() => console.log('WebView loading started')}
-            onLoadEnd={() => console.log('WebView loading finished')}
-            onError={() => console.error('WebView failed to load')}
-        />
-      </SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      <WebView
+        style={styles.webview}
+        originWhitelist={['*']}
+        source={{ uri: 'http://192.168.0.10:5173/' }}
+        onLoad={() => console.log('WebView loaded successfully')}
+        javaScriptEnabled={true}  // JavaScript 활성화
+        domStorageEnabled={true}  // DOM Storage 활성화
+        onLoadStart={() => console.log('WebView loading started')}
+        onLoadEnd={() => console.log('WebView loading finished')}
+        onError={() => console.error('WebView failed to load')}
+      />
+    </SafeAreaView>
+  )
+}
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView
+        style={styles.webview}
+        originWhitelist={['*']}
+        source={{ uri: 'http://192.168.0.10:5173/' }}
+        onLoad={() => console.log('WebView loaded successfully')}
+        javaScriptEnabled={true}  // JavaScript 활성화
+        domStorageEnabled={true}  // DOM Storage 활성화
+        onLoadStart={() => console.log('WebView loading started')}
+        onLoadEnd={() => console.log('WebView loading finished')}
+        onError={() => console.error('WebView failed to load')}
+      />
+    </SafeAreaView>
+    // <NavigationContainer>
+    //   <RootStack.Navigator>
+    //     <RootStack.Screen name="Home" component={WebviewContainer} options={{ headerShown: false }} />
+    //   </RootStack.Navigator>
+    // </NavigationContainer>
   );
 }
 
